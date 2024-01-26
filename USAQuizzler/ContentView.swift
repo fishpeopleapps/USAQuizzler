d9b438c192a7b236b997b49d14e086d788e48b2c
KimmyWingz <127132791+fishpeopleapps@users.noreply.github.com>
2024-01-26 16:47:47 -0700

    assets and icons added

---
+++ USAQuizzler/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  USAQuizzler
+//
+//  Created by Kimberly Brewer on 12/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ USAQuizzler/USAQuizzlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  USAQuizzlerApp.swift
+//  USAQuizzler
+//
+//  Created by Kimberly Brewer on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct USAQuizzlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
